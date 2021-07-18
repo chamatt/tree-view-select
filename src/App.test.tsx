6e37640381210312236bea0e48fb873297d3bd7a
chamatt <matt.vicent@gmail.com>
2021-07-18 03:39:15 -0300

    chore: setting up tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders placeholder", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/placeholder/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+
+const App = () => {
+  return <div className="App">Placeholder</div>;
+};
 
 export default App;
